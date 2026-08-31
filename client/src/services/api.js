146f20720b3f2e0638bd5abc13ac270d530c9b71
Adarsh <adarshkumarrahar@gmail.com>
2026-08-31 14:01:14 +0530

    updated render link

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL : 'https://resume-analyzer-5p0e.onrender.com',
+    baseURL : 'https://resume-analyzer-5p0e.onrender.com/api',
 });
 
 export default api;
